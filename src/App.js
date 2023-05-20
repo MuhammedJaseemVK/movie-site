18e58f890e045cb96a87a0473ea00afe6f13c01f
Jaseem <muhammedjaseemvk96@gmail.com>
2023-05-20 19:39:58 +0530

    hid the API_KEY.fixed a mistake

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const App = () => {
     const searchMovies = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}`);
 
-        const data = await response.json();
+        const data = await response.json(); 
         setMovies(data.Search);
 
         
